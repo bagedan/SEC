516dd8905cc54c0acf8850ee9ad3c91c3b4e1ddc
zxz <zxz@zxzmatoMacBook-Air.local>
2015-12-22 00:45:09 +0800

    add maven plugin

--- StreamingService/src/com/cep/streaming/StreamingRunner.scala
+++ StreamingService/src/com/cep/streaming/StreamingRunner.scala
@@ -2,7 +2,7 @@ package com.cep.streaming
 
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{ Seconds, StreamingContext }
-import com.cep.event.generators.EventPublisher
+//import com.cep.event.generators.EventPublisher
 
 //import com.datastax.bdp.spark.DseSparkConfHelper
 
@@ -10,18 +10,20 @@ object StreamingRunner {
   def main(args: Array[String]): Unit = {
 
     // Create the context with a 1 second batch size
-    
-   val sparkConf = new SparkConf().setAppName("cep-streaming")
-   
-   if(args.length>=1&&"local".equals(args(0))){
+
+    val QUEUE_ID = "EVENTS";
+    val BORKE_URL = "tcp://localhost:61616";
+    val sparkConf = new SparkConf().setAppName("cep-streaming")
+
+    if (args.length >= 1 && "local".equals(args(0))) {
       sparkConf.setMaster("local[*]")
-   }
-   
-   //val sparkConf = DseSparkConfHelper.enrichSparkConf(new SparkConf().setAppName("cep-streaming"));
-    
+    }
+
+    //val sparkConf = DseSparkConfHelper.enrichSparkConf(new SparkConf().setAppName("cep-streaming"));
+
     val ssc = new StreamingContext(sparkConf, Seconds(1))
 
-    val lines = ssc.receiverStream(new ActiveMQReveiver(EventPublisher.BORKE_URL, EventPublisher.QUEUE_ID))
+    val lines = ssc.receiverStream(new ActiveMQReveiver(BORKE_URL, QUEUE_ID))
     //lines.filter(x => x.startsWith("Message"))
     lines.print()
 
